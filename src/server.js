fee0e175d5ac797a765c68b1bb0672f8429a42b4
phenomenonlee <flejdud8@naver.com>
2022-03-28 18:18:43 +0900

    req, res change

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 7000;
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send("i still love you");
+}
+
+const handleLogin = (req,res) =>{
+    return res.send("Login here");
+}
+app.get("/",handleHome);
+app.get("/login",handleLogin)
+
+const handleListening = () =>
+console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT,handleListening);
