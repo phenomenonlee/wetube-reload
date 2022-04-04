9f1ad2509be44db5cdb911524198291804529b47
phenomenonlee <flejdud8@naver.com>
2022-04-04 19:47:21 +0900

    update pug 2
    하나의 파일로 모든 페이지를 업데이트 할 수 있는 방법이 있다.

--- src/server.js
+++ src/server.js
@@ -11,8 +11,11 @@ console.log(process.cwd());
 const app = express(); 
 const logger = morgan("dev");
 
+
 app.set("view engine", "pug")
+app.set("views",process.cwd() + "/src/views");
 app.use(logger);
+
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
