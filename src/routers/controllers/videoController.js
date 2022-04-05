26ab267df2feff618db2f79c1d285da0ee542d26
phenomenonlee <flejdud8@naver.com>
2022-04-05 17:12:03 +0900

    inheritance extends
    base 파일을 기반으로 해서 나머지 pug파일을 확장할 수 있게 해준다.

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -3,9 +3,7 @@ export const trending = (req, res) => res.render("home");
 export const see = (req,res) => res.render("watch");
 
 export const search = (req,res) =>res.send("Search");
-export const edit = (req,res) =>{
-    return res.send("Edit");
-}
+export const edit = (req,res) =>res.render("edit");
 export const deleteVideo = (req,res) =>{
     return res.send("Delete Video");
 }
