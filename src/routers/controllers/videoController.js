e317fdca06d49bd66225ea4afec1f3fb19176b24
phenomenonlee <flejdud8@naver.com>
2022-04-05 17:32:31 +0900

    variable template
    반복되는 부분 중에서 변하는 부분만을 변수를 지정해서 더 쉽게 만들 수 있다.

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -1,10 +1,10 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
 
 export const see = (req,res) => res.render("watch");
 
 export const search = (req,res) =>res.send("Search");
 export const edit = (req,res) =>res.render("edit");
-export const deleteVideo = (req,res) =>{
+export const deleteVideo = (req,res) =>{ 
     return res.send("Delete Video");
 }
 export const upload = (req,res) =>res.send("Upload");
