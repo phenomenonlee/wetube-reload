3c19d688c936dafdf4c4aaa2cc83987bc85a7a6f
phenomenonlee <flejdud8@naver.com>
2022-04-12 17:03:09 +0900

    2022-04-12 #6.11 Our First Query
    각 역할에 맞게 파일을 분리시켰다.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 5000;
 
 const app = express();
 const logger = morgan("dev");
@@ -20,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
