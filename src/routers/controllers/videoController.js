304e32ead23abe46313e0a676578c6b844820f1a
phenomenonlee <flejdud8@naver.com>
2022-04-19 17:19:22 +0900

    2022-04-19 #6.24 Statics
    static은 우리가 직접 함수를 만들어서 모델에게 쓸 수 있게 해준다. 엄청난 것 같다.

--- src/routers/controllers/videoController.js
+++ src/routers/controllers/videoController.js
@@ -43,9 +43,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
 
   return res.redirect(`/videos/${id}`);
@@ -60,7 +58,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
