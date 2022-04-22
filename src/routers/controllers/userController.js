91df07b92e7418cfd7452a1c1fedbf76706439bc
phenomenonlee <flejdud8@naver.com>
2022-04-22 15:42:05 +0900

    2022-04-22 #7.5 Login part One
    login page를 만들고 username (아이디)가 있는지 체크했다.

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -1,3 +1,4 @@
+import res from "express/lib/response";
 import User from "../../models/User";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
@@ -19,18 +20,42 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken.",
     });
   }
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle,
+      errorMessage: error._message,
+    });
+  }
+};
+
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const exists = await User.exists({ username });
+  if (!exists) {
+    return res
+      .status(400)
+      .render("login", {
+        pageTitle: "Login",
+        errorMessage: "An account with this username does not exists.",
+      });
+  }
+  //check if account exists
+  //check if password correct
+  res.end();
 };
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
