77ff7f160d5074bbea48c031c164d6fce19318c4
phenomenonlee <flejdud8@naver.com>
2022-04-25 17:05:59 +0900

    2022-04-25 #7.9 Logged In User
    우리는 각 세션 DB에 있는 정보를 넣을 수 있게 되었다.

--- src/server.js
+++ src/server.js
@@ -31,7 +31,6 @@ app.get("/add-one", (req, res, next) => {
   req.session.potato += 1;
   return res.send(`${req.session.id}\n${req.session.potato}`);
 });
-
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
