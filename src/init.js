90699d4f409f7bc0000cff69bb9b691af5d62c92
phenomenonlee <flejdud8@naver.com>
2022-04-28 17:23:25 +0900

    2022-04-28 #7.12 MongoStore
    connetion-mongo를 설치해서 MongoDB와 session store를 연결시켰다.

--- src/init.js
+++ src/init.js
@@ -3,7 +3,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 5000;
+const PORT = 7000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
