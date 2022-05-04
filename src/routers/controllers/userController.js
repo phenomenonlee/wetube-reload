facedd4b329a45b59dfd4a8f673f411f3444b86b
phenomenonlee <flejdud8@naver.com>
2022-05-04 20:05:44 +0900

    2022-05-04 #7.22 Log Out
    git 로그인과 로그아웃을 했는데 session destroy로 로그아웃을 했다.git 로그인 섹션은 다시 한번 공부를 해야 될 것 같다. fetch 부분도 개념을 다시 공부해야 될 것 같다.

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -43,7 +43,7 @@ export const getLogin = (req, res) =>
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
   const pageTitle = "Login";
-  const user = await User.findOne({ username });
+  const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
     return res.status(400).render("login", {
       pageTitle,
@@ -115,13 +115,10 @@ export const finishGithublogin = async (req, res) => {
     if (!emailObj) {
       return res.redirect("/login");
     }
-    const existingUser = await User.findOne({ eamil: emailObj.email });
-    if (existingUser) {
-      req.session.loggedIn = true;
-      req.session.user = existingUser;
-      return res.redirect("/");
-    } else {
-      const user = await User.create({
+    let user = await User.findOne({ eamil: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        avatarUrl: userData.avatarUrl,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -129,16 +126,18 @@ export const finishGithublogin = async (req, res) => {
         socialOnly: true,
         location: userData.location,
       });
-      req.session.loggedIn = true;
-      req.session.user = user;
-      return res.redirect("/");
     }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
   } else {
     return res.redirect("/login");
   }
 };
 
+export const logout = (req, res) => {
+  req.session.destroy();
+  return res.redirect("/");
+};
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
-export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,7 +1,6 @@
 import express from "express";
 import {
   edit,
-  remove,
   logout,
   see,
   startGithublogin,
@@ -12,7 +11,6 @@ const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
 userRouter.get("/github/start", startGithublogin);
 userRouter.get("/github/finish", finishGithublogin);
 userRouter.get("/id", see);
