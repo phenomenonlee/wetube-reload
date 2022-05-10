7911253076c41f2f7879280ae226ba95c2deea3c
phenomenonlee <flejdud8@naver.com>
2022-05-10 19:10:50 +0900

    2022-05-10 #8.6 File Uploads part One
    이게 진짜 8-6이고 저번 강의가 8-5이다.

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -151,8 +151,9 @@ export const postEdit = async (req, res) => {
       user: { _id },
     },
     body: { name, email, username, location },
+    file,
   } = req;
-
+  console.log(file);
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
