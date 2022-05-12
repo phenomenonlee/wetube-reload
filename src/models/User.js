53d90f3136349658a1f63613261a2e031eb59433
phenomenonlee <flejdud8@naver.com>
2022-05-13 00:31:07 +0900

    2022-05-13 #8.14 Bugfix

--- src/models/User.js
+++ src/models/User.js
@@ -11,9 +11,10 @@ const userSchema = new mongoose.Schema({
   location: String,
   videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
-
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
 
 const User = mongoose.model("User", userSchema);
