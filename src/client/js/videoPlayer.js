96c95e2445fca3bdb23cc4f3bc2bc6c4b1780e43
phenomenonlee <flejdud8@naver.com>
2022-05-16 17:45:49 +0900

    2022-05-16 #11.1 Play Pause
    paly, Mute, time, button과 range를 만들었고 EventListener를 사용해서 click과 inner Text를 사용했다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = (e) => (playBtn.innerText = "play");
+const handlePlay = (e) => (playBtn.innerText = "pause");
+
+const handleMuteClick = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteClick);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
