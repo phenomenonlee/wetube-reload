c11b716b307a89a39fbc961cff0e78222b747fa5
phenomenonlee <flejdud8@naver.com>
2022-05-17 14:37:02 +0900

    2022-05-17 #11.2 Mute and Unmute
    음소거 버튼을 만들었다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -10,13 +10,18 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = (e) => (playBtn.innerText = "play");
-const handlePlay = (e) => (playBtn.innerText = "pause");
 
-const handleMuteClick = (e) => {};
+const handleMuteClick = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
