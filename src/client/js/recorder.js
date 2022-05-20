5e7df4892dd04a5bfb7f16060335d9f534ea35b2
phenomenonlee <flejdud8@naver.com>
2022-05-21 02:23:22 +0900

    2022-05-20 #13.1 Video Preview
    srcObject는 MediaStream, mediaSource, Blob, File을 실행할 때 video에 주는 무언가를 의미하기도 한다. HTMl 요소인 src와는 다른 것이다.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true,
+    video: { with: 400, height: 400 },
   });
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
