7106ef8f71d5cde784f934d1c6d0285b90841dcc
phenomenonlee <flejdud8@naver.com>
2022-05-21 03:14:10 +0900

    2022-05-20 #13.2 Recording Video
    recorder.stop()을 호출하게 되면 이 dataavialable event가 발생한다는 것이다. 그리고 이 event는 최종 비디오 파일과 함께 딸려 나온다. 그래서 우린 ondataavailable handler에 event를 등록해주어야 한다.

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,37 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let = stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recordin";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: { with: 400, height: 400 },
   });
   video.srcObject = stream;
   video.play();
 };
+init();
 
 startBtn.addEventListener("click", handleStart);
