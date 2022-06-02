dc3b409159e6c346bd1bd3c0711d93a794873898
phenomenonlee <flejdud8@naver.com>
2022-06-02 19:17:01 +0900

    isHeroku

--- src/routers/controllers/userController.js
+++ src/routers/controllers/userController.js
@@ -154,10 +154,11 @@ export const postEdit = async (req, res) => {
     body: { name, email, username, location },
     file,
   } = req;
+  const isHeroku = process.env.NODE_ENV === "prodution";
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.location : avatarUrl,
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       username,
